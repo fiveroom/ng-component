7d264ae0e25a7e60799fd0cb3e79fff3b7474628
zhou_xi_cheng <lkzipsea@163.comcccc>
2022-03-02 18:26:16 +0800

    feat: 导出lib打包

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ require('ts-node').register({
     project: './script/tsconfig.json'
 });
 
-const { buildLess } = require('./script/gulpfile');
+const {buildLess} = require('./script/gulpfile');
+const {buildLib} = require('./script/buildLib')
 
 module.exports = {
-    buildLess
+    buildLess,
+    buildLib
 }
